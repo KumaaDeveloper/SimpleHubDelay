6f7692cfca236e9aec23f23cdc075393004f9bbc
FunkaRevon <ardrafunka@gmail.com>
2024-06-07 23:49:51 +0700

    Add files via upload

--- src/BeeAZZ/SimpleHubDelay/Main.php
+++ src/BeeAZZ/SimpleHubDelay/Main.php
@@ -6,25 +6,46 @@ namespace BeeAZZ\SimpleHubDelay;
 use pocketmine\command\{Command, CommandSender};
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\Task;
 
 class Main extends PluginBase{
 
-	public function onEnable() : void{
-		$this->saveDefaultConfig();
-	}
+    public function onEnable() : void{
+        $this->saveDefaultConfig();
+    }
 
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-		if($command->getName() == "hub"){
-			if(!$sender instanceof Player){
-				$sender->sendMessage($this->getConfig()->get("message-useingame"));
-				return true;
-			}
-			if($sender->hasPermission("simplehubdelay.command")){
-				$message = str_replace("{SECONDS}", $this->getConfig()->get("delay"), $this->getConfig()->get("message-delay"));
-				$sender->sendMessage($message);
-				$this->getScheduler()->scheduleDelayedTask(new UpdateTask($this, $sender), 20 * (int)$this->getConfig()->get("delay"));
-			}
-		}
-		return true;
-	}
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        if(in_array($command->getName(), ["hub", "lobby", "spawn"])){
+            if(!$sender instanceof Player){
+                $sender->sendMessage($this->getConfig()->get("message-useingame"));
+                return true;
+            }
+            if($sender->hasPermission("simplehubdelay.command")){
+                $delay = $this->getConfig()->get("delay");
+                $message = str_replace("{SECOND}", (string)$delay, $this->getConfig()->get("message-delay"));
+                $sender->sendMessage($message);
+                $this->getScheduler()->scheduleDelayedTask(new UpdateTask($this, $sender), 20 * (int)$delay);
+            }
+        }
+        return true;
+    }
 }
+
+class UpdateTask extends Task{
+
+    protected Main $main;
+    protected Player $player;
+
+    public function __construct(Main $main, Player $player){
+        $this->main = $main;
+        $this->player = $player;
+    }
+
+    public function onRun() : void{
+        if($this->player->isOnline()){
+            $this->player->teleport($this->main->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+            $this->player->sendMessage($this->main->getConfig()->get("message-success"));
+        }
+    }
+}
+?>
